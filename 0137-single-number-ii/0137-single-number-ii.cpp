6b67bb090a7fbdc2c8a593db40042d303a4a3059
vishal kumar <145883709+Vishal-lnct@users.noreply.github.com>
2026-03-22 15:32:33 +0530

    Time: 2 ms (39.04%), Space: 13.5 MB (35.19%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int res=0;
+        for(int k=0;k<=31;k++){
+    
+            int temp=1<<k;
+            int ones=0;
+            for(int num:nums){
+                if((num&temp)==0){
+
+                }else{
+                    ones++;
+                }
+            }
+if(ones%3==1){
+            res=(res|temp);
+
+        }
+        
+
+        }
+        return res;
+    }
+};
