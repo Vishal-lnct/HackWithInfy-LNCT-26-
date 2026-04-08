24fcb21c14cd596ffad70e1546ffe4ee1eb9f4ec
vishal kumar <145883709+Vishal-lnct@users.noreply.github.com>
2026-04-08 23:59:28 +0530

    Time: 0 ms (100%), Space: 12 MB (45.05%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& s) {
+        if(s.empty()){
+            return"";
+        }
+        string prefix=s[0];
+
+        for(int i=1;i<s.size();i++){
+            while(s[i].find(prefix)!=0){
+                prefix=prefix.substr(0,prefix.length()-1);
+                 if(prefix.empty()){
+            return"";
+            }
+        }
+        }
+return prefix;
+    }
+};
