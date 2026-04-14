300c7f8f3fbae41d1db4cf2c3806fbcbbcbd8475
vishal kumar <145883709+Vishal-lnct@users.noreply.github.com>
2026-04-14 15:24:36 +0530

    Time: 2846 ms (5.03%), Space: 14.5 MB (94.36%) - LeetHub

--- 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
+++ 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
@@ -8,12 +8,8 @@ public:
            int x=0;
            if(nums1[i]==nums2[j]){
             
-               while(i+x<nums1.size() && j+x<nums2.size() &&nums1[i+x]==nums2[j+x] ){
-                   if(nums1[i+x]==nums2[j+x]){
-                       x++;
-                   }else{
-                    continue;
-                   }
+               while(i+x<nums1.size() && j+x<nums2.size()&&nums1[i+x]==nums2[j+x]){
+                  x++;
                }
                
                 
