7cb8ddfb9c1a0619d00069f6e091f01fd7105a7b
vishal kumar <145883709+Vishal-lnct@users.noreply.github.com>
2026-04-26 15:02:32 +0530

    Time: 2 ms (62.86%), Space: 136.8 MB (65.83%) - LeetHub

--- 3914-minimum-operations-to-make-array-non-decreasing/3914-minimum-operations-to-make-array-non-decreasing.cpp
+++ 3914-minimum-operations-to-make-array-non-decreasing/3914-minimum-operations-to-make-array-non-decreasing.cpp
@@ -6,7 +6,7 @@ public:
        for(int i=1;i<nums.size();i++){
         if(nums[i]<nums[i-1]){
             x+=nums[i-1]-nums[i];
-            nums[i]-nums[i-1];
+            // nums[i]=nums[i-1];
         }
        }
        return x;
