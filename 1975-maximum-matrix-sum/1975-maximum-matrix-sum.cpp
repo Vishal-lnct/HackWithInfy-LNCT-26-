1da024e53966aa9ed65807ac38d35fe7b833c4a4
vishal kumar <145883709+Vishal-lnct@users.noreply.github.com>
2026-04-29 23:36:53 +0530

    Time: 6 ms (11.62%), Space: 39.3 MB (26.84%) - LeetHub

--- 1975-maximum-matrix-sum/1975-maximum-matrix-sum.cpp
+++ 1975-maximum-matrix-sum/1975-maximum-matrix-sum.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
-    long long maxMatrixSum(vector<vector<int>>& matrix) {
+    long long maxMatrixSum(vector<vector<int>>& mat) {
+        
 
-         long long sum = 0;
-         int neg = 0;
-         int mn = INT_MAX;
+        int row=mat.size();
+        int col=mat[0].size();
 
-        for (int i = 0; i < matrix.size(); i++) {
-            for (int j = 0; j < matrix[i].size(); j++) {
-                int x = matrix[i][j];
-                sum += abs(x);
-                if (x < 0) neg++;
-                mn = min(mn, abs(x));
+        long long  sum=0;
+        int x=0;
+        int mn=INT_MAX;
+
+        for(int i=0;i<mat.size();i++){
+            for(int j=0;j<mat.size();j++){
+                sum+=abs(mat[i][j]);
+                if(mat[i][j]<0){
+                    x++;
+                }
+                mn=min(mn,abs(mat[i][j]));
             }
         }
-
-        if (neg % 2 != 0)
-            sum -= 2 * mn;
-
-        return sum;
-        
+        if(x%2==0){
+            return sum;
+        }else{
+            return sum-2*mn;
+        }
     }
 };
