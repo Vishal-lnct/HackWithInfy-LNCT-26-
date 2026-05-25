eb981a72248e57df1b6c2718f311adc15a82dd69
vishal kumar <145883709+Vishal-lnct@users.noreply.github.com>
2026-05-25 20:47:01 +0530

    Time: 3 ms (76.45%), Space: 10.1 MB (78.61%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+
+int check(int i,string s){
+
+    if(s[i]=='I'){
+        return 1;
+    }else if(s[i]=='V'){
+        return 5;
+    }else if(s[i]=='L'){
+        return 50;
+    }else if(s[i]=='C'){
+        return 100;
+    }else if(s[i]=='D'){
+        return 500;
+    }else if(s[i]=='M'){
+        return 1000;
+    }else if(s[i]=='X'){
+        return 10;
+    }
+    return 0;
+}
+    int romanToInt(string s) {
+int n=s.size()-1;
+     int sum=0;
+
+     int i=0;
+     while(i<s.size()){
+    if(check(i,s)<check(i+1,s)){
+        sum-=check(i,s);
+    }else{
+        sum+=check(i,s);
+    }
+    i++;
+     }
+
+// sum+=check(n-1,s);
+return sum;
+    }
+};
