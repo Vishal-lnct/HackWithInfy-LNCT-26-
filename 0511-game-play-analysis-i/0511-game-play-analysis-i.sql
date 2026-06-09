223d1b0233010c5d66cb6a4a84522ab58fa6bfa2
vishal kumar <145883709+Vishal-lnct@users.noreply.github.com>
2026-06-09 10:47:22 +0530

    Time: 520 ms (38.38%), Space: 0B (100%) - LeetHub

--- 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
+++ 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
@@ -1,5 +1,5 @@
-select player_id, MIN(event_date) as first_login
-
-from Activity
-
-group by  player_id;
+SELECT
+    player_id,
+    MIN(event_date) AS first_login
+FROM Activity
+GROUP BY player_id;
