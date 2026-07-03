1c9d4c0c1d6bc5d4750ccaf35e1889969555f8b6
vishal kumar <145883709+Vishal-lnct@users.noreply.github.com>
2026-07-03 23:37:56 +0530

    Time: 22 ms (56.84%), Space: 85.9 MB (20.68%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,59 +1,81 @@
 class Solution {
 public:
-vector<int>solve1(vector<int>& heights){
+
+void Left(vector<int>&heights,vector<int>&left){
+
+    
     int n=heights.size();
 
-    vector<int>ans(n,-1);
+   left.assign(n, -1);
     stack<int>st;
+
     for(int i=0;i<n;i++){
+
+
         while(!st.empty() && heights[st.top()]>=heights[i]){
             st.pop();
         }
+
         if(!st.empty()){
-            ans[i]=st.top();
+            left[i]=st.top();
         }
         st.push(i);
 
     }
-    return ans;
 }
 
-vector<int>solve2(vector<int>& heights){
+
+void Right(vector<int>&heights,vector<int>&right){
+
+    
     int n=heights.size();
 
-    vector<int>ans(n,n);
+   right.assign(n, n);
     stack<int>st;
+
     for(int i=n-1;i>=0;i--){
+
+
         while(!st.empty() && heights[st.top()]>=heights[i]){
             st.pop();
         }
+
         if(!st.empty()){
-            ans[i]=st.top();
+            right[i]=st.top();
         }
         st.push(i);
 
     }
-    return ans;
 }
-
-
     int largestRectangleArea(vector<int>& heights) {
-int maxi=INT_MIN;
-        vector<int>left=solve1(heights);
-        vector<int>right=solve2(heights);
 
-        for(int i=0;i<heights.size();i++){
-            int l=left[i];
-            int r=right[i];
-            int d=r-l-1;
-            int area=d*heights[i];
+        vector<int>left;
+        vector<int>right;
+Left(heights,left);
 
-            maxi=max(maxi,area);
-        }
+// for(int i=0;i<left.size();i++){
+//     cout<<left[i]<<" ";
+// }
 
+Right(heights,right);
+
+// for(int i=0;i<right.size();i++){
+//     cout<<right[i]<<" ";
+// }
+
+int maxi=0;
+for(int i=0;i<heights.size();i++){
+    // int r=right[i];
+    // int l=left[i];
+
+    
+    int area = heights[i] * (right[i] - left[i] - 1);
+    maxi=max(maxi,area);
+
+}
 
 
 return maxi;
-        
+    return 0;    
     }
 };
