c7a4a2a74623db9e05eacae56e383fb878328665
vishal kumar <145883709+Vishal-lnct@users.noreply.github.com>
2026-07-03 18:35:44 +0530

    Time: 0 ms (100%), Space: 9 MB (9.03%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int length = 0;
-    int i = s.length() - 1;
-
-   
-    while (i >= 0 && s[i] == ' ') {
-        i--;
-    }
-
-  
-    while (i >= 0 && s[i] != ' ') {
-        length++;
-        i--;
-    }
-
-    return length;
-}
+        int n=s.size();
+        if(n==1){
+            return 1;
+        }
+    // cout<<s[n-1];
+    int j=n-1;
+    while(j>=0 &&s[j]==' ' ){
+        j--;
         
+    }
     
+    int count=0;
+    while(j>=0 && s[j]!=' '  ){
+        count++;
+        
+        j--;
+        
+    }
+    return count;
+    }
 };
