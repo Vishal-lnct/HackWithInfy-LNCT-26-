c47b46e7d7b473229823781dc8dab14e450dda56
vishal kumar <145883709+Vishal-lnct@users.noreply.github.com>
2026-07-03 14:13:47 +0530

    Time: 0 ms (100%), Space: 21.5 MB (66.33%) - LeetHub

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -13,23 +13,17 @@ class Solution {
 public:
 
 bool check(TreeNode* root,int targetSum){
-if(!root){
-            return false;
-        }
 
-        if(!root->left && !root->right){
-            if(targetSum-root->val==0){
-                return true;
-            }
-        }
-return check(root->left, targetSum-root->val) ||
-       check(root->right, targetSum-root->val);
-        // return false;
+    if(!root){
+        return false;
+    }
 
+    if(!root->left && !root->right && targetSum-root->val==0){
+return true;
+    }
+    return check(root->left,targetSum-root->val)||check(root->right,targetSum-root->val);
 }
     bool hasPathSum(TreeNode* root, int targetSum) {
         return check(root,targetSum);
-        
-        
     }
 };
