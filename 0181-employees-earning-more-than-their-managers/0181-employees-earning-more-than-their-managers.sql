2b1c173e57fc1aebc616520ea0e1815320a933bb
vishal kumar <145883709+Vishal-lnct@users.noreply.github.com>
2026-07-24 22:06:16 +0530

    Time: 419 ms (32.94%), Space: 0B (100%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -1,6 +1,6 @@
-Select e1.name as employee 
-from employee e1
-join employee e2
-on e1.managerId=e2.id
-where e1.salary>e2.salary;
 
+select e1.name as Employee
+from  Employee e1
+join Employee e2
+on e1.managerId=e2.Id
+where e1.salary>e2.salary;
