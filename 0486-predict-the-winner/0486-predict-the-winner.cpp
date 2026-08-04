8a7d6c988964e4b6c07785ac766e98dfa3435797
vishal kumar <145883709+Vishal-lnct@users.noreply.github.com>
2026-08-05 01:48:11 +0530

    Time: 0 ms (100%), Space: 9.7 MB (52.78%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-
+int t[23][23];
 int check(int i,int j,vector<int>&nums){
 
 
@@ -11,16 +11,19 @@ if(i>j){
 if(i==j){
     return nums[i];
 }
+if(t[i][j]!=-1){
+    return t[i][j];
+}
 
 int a=nums[i]-check(i+1,j,nums);
 
 int b=nums[j]-check(i,j-1,nums);
 
-return max(a,b);
+return t[i][j]= max(a,b);
 
 }
     bool predictTheWinner(vector<int>& nums) {
-
+memset(t,-1,sizeof(t));
         int i=0;
         int j=nums.size()-1;
     int p=check(i,j,nums);
