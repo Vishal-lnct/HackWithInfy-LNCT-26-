4b892d8467c9f740f9aa975de8dc160705d73d87
vishal kumar <145883709+Vishal-lnct@users.noreply.github.com>
2026-08-04 18:14:21 +0530

    Time: 5 ms (80.99%), Space: 45.6 MB (94.74%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+  if (x < 0) {
+            return false;
+        }
+        int  p=x;
+int rem;
+int ans=0;
+       while(x!=0){
+         rem=x%10;
+        ans=ans*10+rem;
+        x=x/10;
+       }
+       if(p==ans){
+        return true;
+       }
+        return false;
+    }
+}
