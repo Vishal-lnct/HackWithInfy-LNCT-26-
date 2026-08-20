6b61d24d6a4c8b6a69ead8cd8473831c0c3d1a18
vishal kumar <145883709+Vishal-lnct@users.noreply.github.com>
2026-08-20 15:12:41 +0530

    Time: 28 ms (48.42%), Space: 16.8 MB (61.53%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,35 +1,42 @@
 class Solution {
 public:
 
-void check(vector<vector<char>>& grid,int i,int j,vector<vector<bool>>&vis){
-int  m=grid.size();
-        int n=grid[0].size();
+void check(int i,int j,vector<vector<char>>& grid ,vector<vector<bool>>&vis){
+int m=grid.size();
+int n=grid[0].size();
+if(i<0 || i>=m ||j<0||j>=n||vis[i][j]==true||grid[i][j]!='1'){
+    return;
 
-        if(i<0 ||i>=m ||j<0 ||j>=n||vis[i][j]||grid[i][j]!='1'){
-            return;
-        }
-        vis[i][j]=true;
-        check(grid,i+1,j,vis);
-        check(grid,i,j+1,vis);
-        check(grid,i-1,j,vis);
-        check(grid,i,j-1,vis);
-
+}
+vis[i][j]=true;
+check(i+1,j,grid,vis);
+check(i,j+1,grid,vis);
+check(i-1,j,grid,vis);
+check(i,j-1,grid,vis);
 
 
 }
     int numIslands(vector<vector<char>>& grid) {
-        int  m=grid.size();
-        int n=grid[0].size();
-   vector<vector<bool>> vis(m, vector<bool>(n, false));
+int m=grid.size();
+int n=grid[0].size();
+vector<vector<bool>>vis(m,vector<bool>(n,false));
 int count=0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-if(grid[i][j]=='1' && vis[i][j]==false){
-    count++;
-    check(grid,i,j,vis);
-}
-            }
+for(int i=0;i<m;i++){
+    for(int j=0;j<n;j++){
+        if(vis[i][j]==false && grid[i][j]=='1'){
+            count++;
+            check(i,j,grid,vis);
+
         }
+    }
+}
+
+
+
+
+
 return count;
+
+        
     }
 };
