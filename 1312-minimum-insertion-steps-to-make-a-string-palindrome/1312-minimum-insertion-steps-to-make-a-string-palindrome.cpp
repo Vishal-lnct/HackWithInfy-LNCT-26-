1aeead7e64b4cb83ac1b0d8ff721d500a18542f4
vishal kumar <145883709+Vishal-lnct@users.noreply.github.com>
2026-08-20 11:59:00 +0530

    Time: 11 ms (98.51%), Space: 10.1 MB (81.51%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+
+int t[501][501];
+int check(string& s,int i,int j){
+    if(i>=j){
+        return 0;
+    }
+if(t[i][j]!=-1){
+    return t[i][j];
+}
+    if(s[i]==s[j]){
+        return t[i][j]= check(s,i+1,j-1);
+    }else{
+        return t[i][j]= 1+min(check(s,i+1,j),check(s,i,j-1));
+    }
+
+}
+    int minInsertions(string s) {
+
+        memset(t,-1,sizeof(t));
+       int j=s.size()-1;
+       return check(s,0,j);
+
+        
+    }
+};
