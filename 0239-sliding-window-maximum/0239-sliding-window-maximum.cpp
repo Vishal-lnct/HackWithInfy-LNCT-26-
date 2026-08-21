f79665d91a9256822378b3822553d00e76dd4d7d
vishal kumar <145883709+Vishal-lnct@users.noreply.github.com>
2026-08-21 21:38:53 +0530

    Time: 21 ms (64.79%), Space: 139.3 MB (49.28%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int>deq;
+
+        vector<int>ans;
+        for(int i=0;i<nums.size();i++){
+
+            while( !deq.empty()&&deq.front()<i-k+1){
+                deq.pop_front();
+            }
+
+            while( !deq.empty()&&nums[i]>nums[deq.back()]){
+              deq.pop_back();
+            }
+
+            deq.push_back(i);
+            if(i>=k-1){
+            ans.push_back(nums[deq.front()]);
+            }
+        }
+        return ans;
+    }
+};
