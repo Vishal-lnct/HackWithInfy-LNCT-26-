f718a30529db0dc71a0cec2b6fe43ed3f47d7eda
vishal kumar <145883709+Vishal-lnct@users.noreply.github.com>
2026-08-27 23:24:02 +0530

    Time: 0 ms (100%), Space: 8.8 MB (80.22%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -1,83 +1,33 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
+        int carry=0;
+        string ans;
 
-               int m = a.size() - 1;
-        int n = b.size() - 1;
+        int m=a.size()-1;
+        int n=b.size()-1;
 
-        int carry = 0;
-        string ans = "";
+        while(m>=0 ||n>=0){
 
-        while (m >= 0 || n >= 0) {
-
-            char x = '0';
-            char y = '0';
-
-            if (m >= 0) {
-                x = a[m];
+            int sum=carry;
+            if(m>=0){
+                sum+=a[m]-'0';
                 m--;
             }
 
-            if (n >= 0) {
-                y = b[n];
+            if(n>=0){
+                sum+=b[n]-'0';
                 n--;
             }
 
-          
-            if (x == '0' && y == '0' && carry == 0) {
-                ans += '0';
-                carry = 0;
-            }
+ans+=(sum%2)+'0';
+carry=sum/2;
 
-          
-            else if (x == '0' && y == '1' && carry == 0) {
-                ans += '1';
-                carry = 0;
-            }
-
-          
-            else if (x == '1' && y == '0' && carry == 0) {
-                ans += '1';
-                carry = 0;
-            }
-
-        
-            else if (x == '1' && y == '1' && carry == 0) {
-                ans += '0';
-                carry = 1;
-            }
-
-         
-            else if (x == '0' && y == '0' && carry == 1) {
-                ans += '1';
-                carry = 0;
-            }
-
-           
-            else if (x == '0' && y == '1' && carry == 1) {
-                ans += '0';
-                carry = 1;
-            }
-
-          
-            else if (x == '1' && y == '0' && carry == 1) {
-                ans += '0';
-                carry = 1;
-            }
-
-         
-            else if (x == '1' && y == '1' && carry == 1) {
-                ans += '1';
-                carry = 1;
-            }
         }
-
-        if (carry == 1) {
-            ans += '1';
+        if(carry>0){
+            ans+='1';
         }
-
-        reverse(ans.begin(), ans.end());
-
+        reverse(ans.begin(),ans.end());
         return ans;
         
     }
