cce8d4b2fc5f517363a23b767994dbd2b9c4c128
vishal kumar <145883709+Vishal-lnct@users.noreply.github.com>
2026-09-02 14:21:58 +0530

    Time: 2 ms (5.1%), Space: 8.5 MB (39.18%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-int check(int i,int n ,vector<int>&dp){
-if(i==n){
-    return 1;
-}
-if(i>n){
-    return 0;
-}
-if(dp[i]!=-1){
-    return dp[i];
-}
-int a =check(i+1,n,dp);
-int b=check(i+2,n,dp);
- return dp[i]=a+b;
- 
 
+int count(int i,int n,vector<int>&dp){
+    if(i>=n){
+        return 1;
+
+    }
+    if(dp[i]!=-1){
+        return dp[i];
+    }
+
+    int a=count(i+1,n,dp);
+    int b=count(i+2,n,dp);
+    return  dp[i]=a+b;
 }
     int climbStairs(int n) {
-        vector<int>dp(n+1,-1);
 
-        return check(0,n,dp);
+        vector<int>dp(n+1,-1);
+        return count(1,n,dp);
     }
 };
