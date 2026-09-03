2537884febd2c7bb7d79766cd74a71ac5d584324
vishal kumar <145883709+Vishal-lnct@users.noreply.github.com>
2026-09-03 10:27:02 +0530

    Time: 0 ms (100%), Space: 9.1 MB (18.28%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -3,30 +3,20 @@ public:
     bool isValid(string s) {
 
         stack<char>st;
+
         for(int i=0;i<s.size();i++){
 
 
-            if(s[i]=='(' || s[i]=='[' || s[i]=='{'){
-                st.push(s[i]);
-            }else{
+            if( !st.empty() && (s[i]=='}' && st.top()=='{' || s[i]==']'  && st.top()=='[' || s[i]==')' && st.top()=='(')){
+                st.pop();
 
-                if(st.empty()){
-                    return false;
-                }else if(s[i]==')' && st.top()=='('){
-                    st.pop();
-                }else if(s[i]==']' && st.top()=='['){
-                    st.pop();
-                }else if(s[i]=='}' && st.top()=='{'){
-                    st.pop();
-                }else{
-                    return false;
-                }
+            }else{
+                st.push(s[i]);
             }
         }
-        if(st.empty()){
-            return true;
+        if(!st.empty()){
+            return false;
         }
-
-        return false;
+        return true;
     }
 };
