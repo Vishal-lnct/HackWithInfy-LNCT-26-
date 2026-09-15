8b5e157fea617aced73adead5ebc091e7d36a48c
vishal kumar <145883709+Vishal-lnct@users.noreply.github.com>
2026-09-15 20:10:22 +0530

    Time: 0 ms (100%), Space: 9.3 MB (25.29%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -2,20 +2,19 @@ class Solution {
 public:
     char findTheDifference(string s, string t) {
 
-        vector<int>x(26,0);
+        vector<int>p(26);
         for(int i=0;i<t.size();i++){
-            x[t[i]-'a']++; 
+p[t[i]-'a']++;
+        }
+                for(int i=0;i<s.size();i++){
+p[s[i]-'a']--;
         }
 
-        for(int i=0;i<s.size();i++){
-            x[s[i]-'a']--;
+        for(int i=0;i<26;i++){
+            if(p[i]>0){
+                return i+'a';
+            }
         }
-
-        for(int i=0;i<x.size();i++){
-         if(x[i]>0){
-            return i+'a';
-         }
-        }
-        return ' ' ;
+        return ' ';
     }
 };
