db7ebcb1777759669983a69c7f98a5099915ddfe
vishal kumar <145883709+Vishal-lnct@users.noreply.github.com>
2026-09-17 09:52:52 +0530

    Time: 0 ms (100%), Space: 14.1 MB (57.63%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
-
 vector<vector<int>>ans;
 
-void check(int i,vector<int>&candidates,int target,vector<int>&temp){
-if(target==0){
-    ans.push_back(temp);
-    return;
-}
+void check(int i,vector<int>& candidates, int target,vector<int>&x){
 
-if(i>=candidates.size() || target<0){
-    return ;
-}
-temp.push_back(candidates[i]);
-check(i,candidates,target-candidates[i],temp);
-temp.pop_back();
-check(i+1,candidates,target,temp);
+    int n=candidates.size();
+    if(i>=n || target<0){
+        return;
+    }
 
+    if(target==0){
+        ans.push_back(x);
+        return;
+    }
+    x.push_back(candidates[i]);
+    check(i,candidates,target-candidates[i],x);
+    x.pop_back();
+    check(i+1,candidates,target,x);
+    
 }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<int>temp;
-        check(0,candidates,target,temp);
+        vector<int>x;
+
+        check(0,candidates,target,x);
         return ans;
+        
     }
 };
