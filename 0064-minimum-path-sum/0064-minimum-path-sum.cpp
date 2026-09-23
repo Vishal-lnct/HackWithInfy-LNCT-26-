b7dd7f667e82ec880f5718cdec288e05c22830c6
vishal kumar <145883709+Vishal-lnct@users.noreply.github.com>
2026-09-23 21:46:18 +0530

    Time: 0 ms (100%), Space: 15.4 MB (82.95%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -2,24 +2,28 @@ class Solution {
 public:
 int t[201][201];
 int check(int i,int j,vector<vector<int>>& grid){
-int m=grid.size();
-int n=grid[0].size();
 
-if(i>=m ||j>=n){
-    return 1e9;
-}
+    int m=grid.size();
+    int n=grid[0].size();
 if(t[i][j]!=-1){
     return t[i][j];
 }
-if(i==m-1 && j==n-1){
-    return grid[m-1][n-1];
-}
-return  t[i][j]= grid[i][j]+min(check(i+1,j,grid),check(i,j+1,grid));
+     if (i == m - 1 && j == n - 1) {
+            return grid[i][j];
+        }
+
+   if (i >= m || j >= n) {
+            return INT_MAX;
+        }
+
+    int a=check(i+1,j,grid);
+    int b=check(i,j+1,grid);
+
+    return  t[i][j]=grid[i][j]+min(a,b);
 
 }
     int minPathSum(vector<vector<int>>& grid) {
-
-        memset(t,-1,sizeof(t));
+ memset(t,-1,sizeof(t));
         return check(0,0,grid);
         
     }
