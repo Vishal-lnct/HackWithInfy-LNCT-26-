b1a49cba1adbb95fa36a574521ec1eac840f94bc
vishal kumar <145883709+Vishal-lnct@users.noreply.github.com>
2026-09-24 17:49:37 +0530

    Time: 4 ms (14.52%), Space: 16.3 MB (40.75%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
-int t[201][201];
-int check(int i,int j,vector<vector<int>>& grid){
-
-    int m=grid.size();
-    int n=grid[0].size();
-if(t[i][j]!=-1){
-    return t[i][j];
-}
-     if (i == m - 1 && j == n - 1) {
-            return grid[i][j];
-        }
-
-   if (i >= m || j >= n) {
-            return INT_MAX;
-        }
-
-    int a=check(i+1,j,grid);
-    int b=check(i,j+1,grid);
-
-    return  t[i][j]=grid[i][j]+min(a,b);
-
-}
     int minPathSum(vector<vector<int>>& grid) {
- memset(t,-1,sizeof(t));
-        return check(0,0,grid);
-        
+
+        int m=grid.size();
+        int n=grid[0].size();
+
+        vector<vector<int>>t(m,vector<int>(n));
+
+        t[0][0]=grid[0][0];
+
+        for(int col=1;col<n;col++){
+
+            t[0][col]=grid[0][col]+t[0][col-1];
+        }
+
+        for(int row=1;row<m;row++){
+
+            t[row][0]=grid[row][0]+t[row-1][0];
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+
+                t[i][j]=grid[i][j]+min(t[i-1][j],t[i][j-1]);
+            }
+        }
+        return t[m-1][n-1];
     }
 };
