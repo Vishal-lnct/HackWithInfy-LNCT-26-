2c1c36e8ceeedff4bb778c85afaf5edc8e095aea
vishal kumar <145883709+Vishal-lnct@users.noreply.github.com>
2026-09-25 10:12:22 +0530

    Time: 0 ms (100%), Space: 8 MB (89.84%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -2,24 +2,24 @@ class Solution {
 public:
 int t[101][101];
 int check(int i,int j,int m,int n){
-    if(i>m-1||j>n-1){
-        return 0;
-    }
-    if(t[i][j]!=-1){
-        return t[i][j];
-    }
 
     if(i==m-1 && j==n-1){
-        return 1;
-    }
+            return 1;
+        }
 
-   int a= check(i+1,j,m,n);
-   int b=check(i,j+1,m,n);
-   return t[i][j]=a+b;
+        if(i>=m || j>=n){
+            return 0;
+        }
+
+        if(t[i][j]!=-1){
+            return t[i][j];
+        }
+        int a=check(i+1,j,m,n);
+        int b=check(i,j+1,m,n);
+        return t[i][j]= a+b;
 }
     int uniquePaths(int m, int n) {
-memset(t,-1,sizeof(t));
-       return check(0,0,m,n);
-        
+        memset(t,-1,sizeof(t));
+       return check(0,0,m,n); 
     }
 };
